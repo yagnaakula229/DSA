a8ad36e8ad475fd1de87b9f673b13b98bdd5d601
yagna <yagnaakula229@gmail.com>
2025-09-15 14:45:43 +0530

    Time: 12 ms (56.45%) | Memory: 42.2 MB (98.01%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character>hm1=new HashMap<>();
-        HashMap<Character,Character>hm2=new HashMap<>();
-        boolean ans=true;
+        HashMap<Character,Character> hm1 = new HashMap<>();
+        HashMap<Character,Character> hm2 = new HashMap<>();
+        
+        if(s.length()!=t.length())
+        return false;
         for(int i=0;i<s.length();i++){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(i);
-            // atfirst a hashmap doesnt contain any element do ch1 is not in hm1 therefore it is added to hm1 
-            if(!hm1.containsKey(ch1) && !hm2.containsKey(ch2)){
-                hm1.put(ch1,ch2);
-                hm2.put(ch2,ch1);
-            }
-            // if ch1 is in hm1 again then  its value should be same as before when it is repeated for first time else it is not isomorphic
-            else if(hm1.containsKey(ch1)&&hm1.get(ch1)!=ch2){
-                ans=false;
-            }
-            else if(hm2.containsKey(ch2)&&hm2.get(ch2)!=ch1){
-                ans=false;
-            }
+            char ch1 = s.charAt(i);
+            char ch2= t.charAt(i);
+            hm1.put(ch1,ch2);
+            hm2.put(ch2,ch1);
         }
-        return ans;
+        if(hm1.size()!=hm2.size()) return false;
+        for(int i=0;i<s.length();i++){
+            char ch1 =s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(ch1!=hm2.get(ch2))
+            return false;
+        }
 
-       
+        return true;
     }
 }
