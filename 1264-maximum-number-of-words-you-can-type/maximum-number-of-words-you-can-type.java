1eb8ad5627ca67ba22db345ed2fc59680c251716
yagna <yagnaakula229@gmail.com>
2025-09-16 00:51:46 +0530

    Time: 2 ms (91.65%) | Memory: 42.6 MB (45.48%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,24 +1,49 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        Stack<String> st = new Stack<>();
         
-        String[] textArr = text.split(" ");
 
-        for(String word:textArr){
-            st.push(word);
-        }
-        int cnt=st.size();
 
-        for(String word:textArr){
-            for(int i=0;i<brokenLetters.length();i++){
-                char ch = brokenLetters.charAt(i);
-                if(word.contains(String.valueOf(ch))){
-                cnt--;
-                break;
-            }
-            }
+
+        
+        // Stack<String> st = new Stack<>();
+        
+        // String[] textArr = text.split(" ");
+
+        // for(String word:textArr){
+        //     st.push(word);
+        // }
+        // int cnt=st.size();
+
+        // for(String word:textArr){
+        //     for(int i=0;i<brokenLetters.length();i++){
+        //         char ch = brokenLetters.charAt(i);
+        //         if(word.contains(String.valueOf(ch))){
+        //         cnt--;
+        //         break;
+        //     }
+        //     }
             
+        // }
+        // return cnt;
+
+
+
+         Set<Character> brokenSet = new HashSet<>();
+        for (char c : brokenLetters.toCharArray()) {
+            brokenSet.add(c);
         }
-        return cnt;
+
+        int count = 0;
+        for (String word : text.split(" ")) {
+            boolean canType = true;
+            for (char c : word.toCharArray()) {
+                if (brokenSet.contains(c)) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) count++;
+        }
+        return count;
     }
 }
