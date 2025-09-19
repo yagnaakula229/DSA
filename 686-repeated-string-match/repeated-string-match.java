2bc86740d181251b247ac27c3971c2c881fe6278
yagna <yagnaakula229@gmail.com>
2025-09-19 16:18:19 +0530

    Time: 530 ms (5.02%) | Memory: 45.2 MB (22.26%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -4,8 +4,17 @@ class Solution {
 
         if(a.contains(b)) return 1;
         //if b has to be in a , then a should be appended minimum of b.length() times 
+        // if a len = 4 , b len =8 then a should repeat 2 times (8/4)
+        // if a len = 3 , b len =8 then we need a to repeat 3 times (8/3)
+        //so we use ceil 
         int minRepeats = (int)Math.ceil((double)(b.length())/a.length()) ;
         String c = "";
+
+            // minRepeats → makes a at least as long as b.
+        // +1 → covers the case where b overlaps across repeat boundaries.
+        // +2 → edge-case safety net (guarantees we don’t miss cases).
+        // \U0001f449 In short:
+        // We do i <= minRepeats + 2 to cover all possible alignments of b inside repeated a, without missing     corner cases.
         for(int i=1;i<=minRepeats+2;i++){
             c=c+a;
             if(c.contains(b)){
