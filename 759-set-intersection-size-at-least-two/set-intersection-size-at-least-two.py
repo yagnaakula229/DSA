d5037d7ae826ded452b78936c6f670571666b46a
yagna <yagnaakula229@gmail.com>
2025-11-20 13:49:11 +0530

    Time: 519 ms (5.52%) | Memory: 18.7 MB (97.93%) - LeetSync

---
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -0,0 +1,16 @@
+class Solution(object):
+   def intersectionSizeTwo(self, intervals):
+       intervals.sort(key=lambda x: (x[0], -x[1]))
+
+       todo = [2] * len(intervals)
+       ans = 0
+
+       while intervals:
+           (s, e), t = intervals.pop(), todo.pop()
+           for p in range(s, s + t):     
+               for i, (s0, e0) in enumerate(intervals):
+                   if todo[i] and p <= e0:
+                       todo[i] -= 1
+               ans += 1
+
+       return ans
