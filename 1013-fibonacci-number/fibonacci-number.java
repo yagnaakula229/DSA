6993d87b8ea198847caaa7fa2baf438a02867c76
yagna <yagnaakula229@gmail.com>
2026-05-20 16:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (32.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -19,35 +19,64 @@
 //method2: Tabulation 
 
 
-//memoization 
-class Solution {
-    // Arrays.fill(dp,-1);
-    public int fib(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-       return solve(n,dp);
-    }
+//memoization  --- TOP DOWN
+// class Solution {
+//     // Arrays.fill(dp,-1);
+//     public int fib(int n) {
+//         int dp[] = new int[n+1];
+//         Arrays.fill(dp,-1);
+//        return solve(n,dp);
+//     }
 
-    public static int solve(int n,int[] dp){
-     if(n==1) return 1;
-     if(n==0) return 0;
-     //if computed , return
-     if(dp[n]!=-1){
-        return dp[n];
-     }
-
-
+//     public static int solve(int n,int[] dp){
+//      if(n==1) return 1;
+//      if(n==0) return 0;
+//      //if computed , return
+//      if(dp[n]!=-1){
+//         return dp[n];
+//      }
 
 
-     // if not computted , compute
 
-        dp[n]=solve(n-1,dp) + solve(n-2,dp);
+
+//      // if not computted , compute
+
+//         dp[n]=solve(n-1,dp) + solve(n-2,dp);
 
         
 
+//         return dp[n];
+
+
+//     }
+
+// }
+
+
+
+// method 2 : tabulation ---- BOTTOM UP
+
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        
+
+
+        dp[0]=0;
+         dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+
+        }
+
         return dp[n];
-
-
+        
     }
 
+
+   
 }
+
