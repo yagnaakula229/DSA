3061c0f0b67710bbe7973f27411cd04b494e7c2b
yagna <yagnaakula229@gmail.com>
2026-05-20 17:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,8 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
+
+        //first step 1 way
         if(n==1){
-            return n;
+            return 1;
         }
+
+        // second step 2 ways
         if(n==2){
             return 2;
         }
@@ -13,16 +17,20 @@ class Solution {
 
     public static int solve(int n , int[] dp){
 
-        if(n<=1){
-            return n;
-        }
+        //dp[0] means total ways to reach first step
         dp[0]=1;
+        //dp[1] means  total ways to reach second step
         dp[1]=2;
+        //dp[2] means total  ways to reach third step 
+        //dp[3] means total  ways to reach fouth step
+        // likewise dp[n-1] means ways to reach nth step;
+
+
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
 
         }
-
+        
         return dp[n-1];
     }
 }
