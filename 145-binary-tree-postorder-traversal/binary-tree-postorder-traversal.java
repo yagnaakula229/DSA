0ea2215de9e8a90890f1c4f90a52c67f0fa66bed
yagna <yagnaakula229@gmail.com>
2026-06-01 20:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (31.41%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -13,21 +13,59 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
 
-    public static List<Integer> postorder(TreeNode root, List<Integer> ls){
-        if(root==null){
-            return ls;
+//     public static List<Integer> postorder(TreeNode root, List<Integer> ls){
+//         if(root==null){
+//             return ls;
+//         }
+
+//         postorder(root.left,ls);
+//         postorder(root.right,ls);
+//         ls.add(root.val);
+
+//         return ls;
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ls = new ArrayList<>();
+//         return postorder(root,ls);
+//     }
+// }
+
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
         }
 
-        postorder(root.left,ls);
-        postorder(root.right,ls);
-        ls.add(root.val);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
 
-        return ls;
-    }
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ls = new ArrayList<>();
-        return postorder(root,ls);
+        st1.push(root);
+
+        while (!st1.isEmpty()) {
+
+            TreeNode node = st1.pop();
+
+            st2.push(node);
+
+            if (node.left != null) {
+                st1.push(node.left);
+            }
+
+            if (node.right != null) {
+                st1.push(node.right);
+            }
+        }
+
+        while (!st2.isEmpty()) {
+            ans.add(st2.pop().val);
+        }
+
+        return ans;
     }
 }
